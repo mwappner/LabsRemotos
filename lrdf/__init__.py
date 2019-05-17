a68987e6a2beccd26de36ec823f66a6c7056efae
mwappner <42619775+mwappner@users.noreply.github.com>
2019-05-17 18:35:26 -0300

    Agrego funcionalidad: si la cámara no está en uso (tomando fotos o videos), está en modo streaming.

---
+++ lrdf/__init__.py
@@ -0,0 +1,268 @@
+from os import listdir, remove, path, getcwd, environ
+from zipfile import ZipFile
+from warnings import catch_warnings
+from flask import Flask, request, send_file, jsonify, Response
+from .device import rangos, nombres, Oscilator, clip_between
+from .utils import utc_later
+
+# Some settings taken from environment variables
+dev_dryrun = os.environ.get('DRYRUN', 'NO') =='YES'
+security_required = os.environ.get('SECURITY', 'NO') =='YES'
+# status_key = {0:'Todo OK', -1:'Valor inválido', -2:'Valor fuera de rango', -3:'Archivo inexistente'}
+
+
+# Create flask app
+app = Flask(__name__)
+
+# Security stuff
+if security_required:
+   from flask_jwt_extended import JWTManager, jwt_required
+
+   app.config['JWT_SECRET_KEY'] = 'super-secret' #change this
+
+   jwt = JWTManager(app)
+   
+   @jwt.expired_token_loader
+   def my_expired_token_callback(expired_token):
+      token_type = expired_token['type']
+      return jsonify({
+         'status': 401,
+         'sub_status': 42,
+         'msg': 'The {} token has expired'.format(token_type)
+      }), 401
+
+else:
+   def jwt_required(func):
+      return func
+
+dev = Oscilator(debug=dev_dryrun)
+
+
+@app.route('/')
+def index():
+    return 'LRDF v2019-5-17'
+
+
+def cambiar_valor(parametro, valor, status=0):
+    '''Intenta cambiar el el valor del parámetro dado. Si se levanta una
+    advertencia, asume que es porque el valor estuvo fuera del rango dado
+    y devuelve el valor correspondiente de status: -2. Faltaría chequear 
+    qué warning se levantó.'''
+    if valor is not None:
+        with catch_warnings(record=True) as w:
+            setattr(dev, parametro, valor)
+            if w: #Es una lista vacía si no hubo warnings
+                status = -2
+    return status, getattr(dev, parametro)
+
+
+def chequear_rango(parametro, valor, status=0, rango=None):
+    '''Chequea que el valor dado es´té en el rango adecuado, según el 
+    parámetro. Si no lo está, avisa usando status=-2 y lo mete en el
+    rango adecuado.'''
+    if rango is None:
+        rango = rangos[parametro]
+    with catch_warnings(record=True) as w:
+        valor = clip_between(valor, *rango)
+        if w: #Es una lista vacía si no hubo warnings
+            status = -2
+    return status, valor
+
+
+@app.route('/rangos')
+def view_rangos():
+    return jsonify(status=0, valor=rangos)
+
+
+@app.route('/parametros')
+def view_parametros():
+    return jsonify(status=0, valor=dev.get_params())
+
+
+@app.route('/encendido')
+def view_encendido():
+    return jsonify(status=0, valor=dev.ison_sound)
+
+
+#@app.route('/enuso_camara')
+#def view_camara():
+#    return jsonify(status=0, valor=dev.ison_cam)
+
+
+@app.route('/frecuencia')
+@app.route('/frecuencia/<float:valor>')
+@app.route('/frecuencia/<int:valor>')
+@jwt_required
+def view_frecuencia(valor=None):
+    status, valor_salida = cambiar_valor('frecuencia', valor)
+    return jsonify(status=status, valor=valor_salida)
+
+
+@app.route('/fase')
+@app.route('/fase/<float:valor>')
+@app.route('/fase/<int:valor>')
+@jwt_required
+def view_fase(valor=None):
+    status, valor_salida = cambiar_valor('fase', valor)
+    return jsonify(status=status, valor=valor_salida)
+
+
+@app.route('/amplitud')
+@app.route('/amplitud/<float:valor>')
+@app.route('/amplitud/<int:valor>')
+@jwt_required
+def view_amplitud(valor=None):
+    status, valor_salida = cambiar_valor('amplitud', valor)
+    return jsonify(status=status, valor=valor_salida)
+
+
+@app.route('/duracion')
+@app.route('/duracion/<float:valor>')
+@app.route('/duracion/<int:valor>')
+@jwt_required
+def view_duracion(valor=None):
+    status, valor_salida = cambiar_valor('duracion', valor)
+    return jsonify(status=status, valor=valor_salida)
+
+
+@app.route('/exposicion')
+@app.route('/exposicion/<float:valor>')
+@app.route('/exposicion/<int:valor>')
+@jwt_required
+def view_exposicion(valor=None):
+    status, valor_salida = cambiar_valor('exposicion', valor)
+    return jsonify(status=status, valor=valor_salida)
+
+
+@app.route('/foto')
+@jwt_required
+def view_foto():
+    file = dev.snapshot()
+    mandar = dict(file=file,
+                 tiempo_estimado=utc_later(10)) #lo hago esperar al menos diez segundos.
+    return jsonify(status=0, valor=mandar)
+
+
+@app.route('/barrido/<int:duracion>/<int:frec_i>/<int:frec_f>')
+@app.route('/barrido/<int:duracion>/<float:frec_i>/<int:frec_f>')
+@app.route('/barrido/<int:duracion>/<int:frec_i>/<float:frec_f>')
+@app.route('/barrido/<int:duracion>/<float:frec_i>/<float:frec_f>')
+@jwt_required
+def hacer_barrido(duracion, frec_i, frec_f):
+    # Chequeo que los valores estén en el rango admitido
+    status, frec_i = chequear_rango('frecuencia', frec_i)
+    status, frec_f = chequear_rango('frecuencia', frec_f, status=status)
+    status, duracion = chequear_rango('duracion', duracion, status=status, rango=(0,60))
+    try:
+        file = dev.video(duracion)
+        dev.sweep(duracion, frec_i, frec_f)
+        mandar = dict(file=file,
+                         tiempo_estimado=utc_later(duracion),
+                         barriendo_entre=[frec_i, frec_f])
+        return jsonify(status=status, valor=mandar)
+
+    except ValueError: #las frecuencias eran incompatibles
+        msg = ('Valores de frecuencias incompatibles. Tal vez frec_i={} igual o más grande que '
+            'frec_f={}, o ambas fuera del rango permitido, en cuyo caso frec_i=frec_f.'
+            ).format(frec_i, frec_f)
+        if status == -2:
+            msg += '\n Frecuencias estaban fuera del rango permitido (status=-2).'
+        return jsonify(status=-1, mensaje=msg)
+
+
+@app.route('/timelapse/<int:frec_i>/<int:frec_f>')
+@app.route('/timelapse/<int:frec_i>/<float:frec_f>')
+@app.route('/timelapse/<float:frec_i>/<int:frec_f>')
+@app.route('/timelapse/<float:frec_i>/<float:frec_f>')
+@jwt_required
+def sacar_timelapse(frec_i, frec_f):
+
+    # Chequeo que los valores estén en el rango admitido
+    status, frec_i = chequear_rango('frecuencia', frec_i)
+    status, frec_f = chequear_rango('frecuencia', frec_f, status)
+
+    try:
+        file = dev.fotos(frec_i, frec_f)
+        mandar = dict(file=file,
+                         tiempo_estimado=utc_later(200), #mucho tiempo extra (debería tardar 100)
+                         barriendo_entre=[frec_i, frec_f])
+        return jsonify(status=status, valor=mandar)
+
+    except ValueError: #las frecuencias eran incompatibles
+        msg = ('Valores de frecuencias incompatibles. Tal vez frec_i={} igual o más grande que '
+            'frec_f={}, o ambas fuera del rango permitido, en cuyo caso frec_i=frec_f.'
+            ).format(frec_i, frec_f)
+        if status == -2:
+            msg += '\n Frecuencias estaban fuera del rango permitido (status=-2).'
+        return jsonify(status=-1, mensaje=msg)
+
+
+@app.route('/getfoto/<path:file>')
+@jwt_required
+def get_ultima_foto(file):
+
+    try:
+        return send_file(path.join(nombres['foto'][0],file))
+    except FileNotFoundError:
+        msg = 'Archivo {} no existente.'.format(file)
+        return jsonify(status=-3, mensaje=msg)
+
+
+@app.route('/getvideo/<path:file>')
+@jwt_required
+def get_video(file):
+    try:
+        return send_file(path.join(nombres['video'][0],file))
+    except FileNotFoundError:
+        msg = 'Archivo {} no existente.'.format(file)
+        return jsonify(status=-3, mensaje=msg)
+
+
+@app.route('/gettimelapse/<path:file>')
+@jwt_required
+def get_timelapse(file):
+    file = path.join(nombres['timelapse'][0], file)
+    try:
+        lista = [path.join(file, f) for f in listdir(file)]
+    except FileNotFoundError:
+        msg = 'Archivo {} no existente.'.format(file)
+        return jsonify(status=-3, mensaje=msg)
+
+    if lista:
+        dev.stop() #para que no siga creando fotos mientras intento mandarlas
+        with ZipFile('send.zip', 'w') as zf:
+            for f in lista:
+                zf.write(f)
+                remove(f)
+
+        dev.play() #vuelvo a arrancar
+        return send_file('send.zip')
+    else:
+        msg = 'Archivo no existente.'
+        return jsonify(status=-3, mensaje=msg)
+
+
+@app.route('/stop')
+@jwt_required
+def stop():
+    dev.stop()
+    return jsonify(status=0)
+
+
+@app.route('/play')
+@jwt_required
+def play():
+    dev.play()
+    return jsonify(status=0)
+
+
+def main(debug=True, browser=False, port=5000):
+    if debug:
+        print(getcwd())
+
+    if browser:
+        import threading, webbrowser
+        url = "http://127.0.0.1:{0}".format(port)
+        threading.Timer(3, lambda: webbrowser.open(url)).start()
+    #app.run(port=port, debug=debug)
+    app.run(host = '0.0.0.0', port=5000, debug=debug)
---
+++ lrdf/device.py
@@ -0,0 +1,200 @@
+from time import sleep, time
+from numpy import linspace
+from threading import Thread
+from os import listdir, remove, path, makedirs
+from shutil import rmtree
+from queue import Empty
+from delegator import run
+from .utils import DeleterQueue, ProcRunning, clip_between, nuevo_nombre, toggle_streaming_concatenar
+
+
+rangos = {
+    'frecuencia': (20, 2000), #Hz
+    'amplitud': (.6, .96), #en escala [0,1]
+    'fase': (-180, 180), #grados
+    'duracion': (0,3600), #segundos
+    'exposicion': (10000, 5000000) #microsegundos
+    }
+iniciales = {
+    'frecuencia': 100, #Hz
+    'amplitud': rangos['amplitud'][-1], #en escala [0,1]
+    'fase': 0, #grados
+    'duracion': 1800, #segundos
+    'exposicion': 30000, #microsegundos
+    }
+replay_when_changed = ['frecuencia',
+                       'amplitud',
+                       #'fase',
+                       'duracion',
+                       ]
+
+#construye rutas completas donde guardar cada tipo de archivo
+constructor = lambda name: path.join('/home/pi/jauretche/LabsRemotos/lvdf/static', name)
+nombres = {
+    'video' : (constructor('videos/'), '.h264'),
+    'foto' : (constructor('fotos/'), '.jpg'),
+    'timelapse' : (constructor('timelapses/'), ''),
+    }
+
+
+class Oscilator:
+    '''A class containing the parameters of the device.'''
+
+    def __setattr__(self, name, value):
+        #Me aseguro de que los valores que deen tener límite 
+        #caigan en el rango adecuado
+        if name in rangos:  
+            value = clip_between(value, *rangos[name])    
+        super().__setattr__(name, value)
+        if name in replay_when_changed and self._initialized:
+            self.play()
+
+    def __init__(self, debug=False):
+        self._initialized = False
+        
+        for key, val in iniciales.items():
+            self.__setattr__(key, val)
+        
+        self._initialized = True
+        self._debug = debug
+
+#        #paro el streaming de la cámara al comienzo, hackfix
+#        if not debug:
+#            run('sudo service motion stop', block=False) 
+
+
+        self.proc_running = dict(cam=ProcRunning(), sound=ProcRunning())
+        self.stopqueue = DeleterQueue(maxsize=1) #acepta cosas per guarda una sola
+
+        self.filequeues = {k:DeleterQueue(accion=remove) for k in nombres}
+        self.filequeues['timelapse'].accion = rmtree #para timelapses necesito otra acción
+        self._existentes() #inicializo las colas con los archivos existentes
+
+        self._timestart_sound = time() #la primera vez, is_on no va a dar bien, pero por lo menos no tira error.
+        self._isplaying = False
+        #self._timestart_cam = time()
+
+    def _existentes(self):
+        #Si estoy en modo debug, no cargo los archivos (suele ser en otro SO)
+        if self._debug:
+            return
+
+        #Inicializo las colas con los archivos existentes, ordenados
+        for cat, cola in self.filequeues.items():
+            #Recupero archivos, ordenados por fecha de creación
+            base = nombres[cat][0]
+            contenido = [path.join(base,f) for f in listdir(base)]
+            contenido.sort(key=lambda f:path.getmtime(f))
+            
+            #Meto todos los archivos en la cola correspondiente
+            for c in contenido:
+                cola.put(c)
+
+    @property
+    def ison_sound(self):
+        return self._timestart_sound + self.duracion > time() and self._isplaying
+
+#    @property
+#    def ison_cam(self):
+#        return self._timestart_cam + self.duration > time()
+
+    def _debugrun(self, command, cat, **kwargs):
+        if self._debug:
+            print(cat, command)
+        else:
+            self.proc_running[cat].run_new(command, **kwargs)
+
+    def play(self):
+        #play with current values
+        self.stopqueue.put(1)
+        run('amixer set PCM -- {}%'.format(self.amplitud*100))
+        command = 'play -n -c1 synth {} sine {}'.format(self.duracion, self.frecuencia)
+        self._debugrun(command, 'sound')
+        self._timestart_sound = time()
+        self._isplaying = True
+
+    def stop(self):
+        for proc in self.proc_running.values():
+            proc.kill()
+        self.stopqueue.put(1)
+        self._isplaying = False
+
+    def get_params(self):
+        return {k:getattr(self, k) for k in rangos}
+            
+    def sweep(self, time, freq_start, freq_end):
+        if freq_start >= freq_end:
+            raise ValueError('Frecuencias incompatibles.')
+        self.stopqueue.put(1)
+        command = 'play -n -c1 synth {} sine {}:{}'.format(time, freq_start, freq_end)
+        self._debugrun(command, 'sound')
+
+    def snapshot(self, file=nuevo_nombre(*nombres['foto']), **kwargs):
+        command = 'raspistill -ss {shutterspeed} -o {file}'.format(
+            shutterspeed = self.exposicion, file = file)
+        command = toggle_streaming_concatenar(command)
+        self._debugrun(command, 'cam', **kwargs)
+        self.filequeues['foto'].put(file)
+        return path.basename(file)
+
+    def video(self, duration):
+        file = nuevo_nombre(*nombres['video'])
+        command = ('raspivid -o {file} -w 960 -h 516 -roi 0,0.5,1,1 -fps 30 ' 
+                   '-ex off -n -br 70 -co 50 -t {dur} -v&').format(
+                           file = file,
+                           dur = duration)
+        command = toggle_streaming_concatenar(command)
+        self._debugrun(command, 'cam')
+        self.filequeues['video'].put(file)
+        return path.basename(file)
+        
+    def fotos(self, freq_start, freq_end):
+        '''Barre cien frecuencais entre freq_start y freq_end. Saca una foto 
+        de larga exposicion a cada frecuencia.'''
+        
+        if freq_start >= freq_end:
+            raise ValueError('Frecuencias incompatibles.')
+
+        #me guardo el estado actual del device
+        d = self.__dict__.copy()
+        
+        self.duracion = 3
+        self.amplitud = 1
+        frecuencias = linspace(freq_start, freq_end, 100)
+        
+        #creo carpeta donde voy a guardar el timelapse y una función para los archivos
+        nombre_base = nuevo_nombre(*nombres['timelapse'])
+        makedirs(nombre_base)
+        nombre = lambda freq: path.join(nombre_base,'{:.1f}Hz.jpg'.format(freq))
+
+        #defino funcion a correr en el thread
+        def accion():
+            for f in frecuencias:
+                self.frecuencia = f #esto pone a andar por 1 segundo
+                sleep(0.1)
+                self.snapshot(nombre(f), block=True)
+                
+                #veo si me indicaron que pare
+                try:
+                    self.stopqueue.get(block=False)
+                except Empty:
+                    continue
+                else:
+                    break
+
+            #AGREGAR UN ELSE ZIP Y MODIFICAR ADECUADAMENTE __init__.py
+            
+            #vuelvo a los valores previos del device
+            for name, value in d.items():
+                setattr(self, name, value)
+        
+        #vacío la cola por si acaso
+        if not self.stopqueue.empty():
+            self.stopqueue.get(block=False)
+
+        #inicializo y prendo el thread
+        fotos_thread = Thread(target=accion)
+        fotos_thread.start()
+
+        self.filequeues['timelapse'].put(nombre_base)
+        return path.basename(nombre_base)
